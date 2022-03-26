f5cc1e3224e7e84b55a43cc4233459daef3fe1b1
Roman Punko <prof2207@mail.ru>
2022-03-26 23:34:38 +0300

    difficult 02

---
+++ script.js
@@ -0,0 +1,22 @@
+let num = 266219;
+let numStr = String(num);
+let arr = Array.from(numStr);
+let i = 0;
+let mult = 1;
+while (i < arr.length) {
+    mult = mult * arr[i];
+    i++;
+}
+console.log(mult);
+
+// пункты 1-2 задания
+
+let mult3 = mult ** 3;
+console.log(mult3);
+
+// пункт з задания
+
+let multStr = String(mult3);
+console.log(multStr.substring(0, 2));
+
+
